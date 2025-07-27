5f182bf3221d5052771f67fd25aeb146ac36a322
Axelhri <axelhouairi@gmail.com>
2025-07-27 16:11:08 +0200

    feat(AuthService): user register

---
+++ src/service/AuthService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AuthService {
+
+    public function __construct(private EntityManagerInterface $entityManager, private UserPasswordHasherInterface $passwordHasher) {}
+
+    public function register(User $user):void {
+        $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+        $this->entityManager->persist($user);
+    }
+}
