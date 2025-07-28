b7dcc98a2d1e77f0c531eb0814c98fcaae598550
Axelhri <axelhouairi@gmail.com>
2025-07-28 18:20:20 +0200

    feat(AuthService): register with dto

--- src/Interface/AuthServiceInterface.php
+++ src/Interface/AuthServiceInterface.php
@@ -2,9 +2,10 @@
 
 namespace App\Interface;
 
+use App\DTO\UserDTO;
 use App\Entity\User;
 
 interface AuthServiceInterface {
-    public function register(User $user):string;
+    public function register(UserDTO $userDTO):string;
     public function login(string $email, string $password):string;
 }
