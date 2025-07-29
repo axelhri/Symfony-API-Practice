26a8a8bbe261cb56639c0a08232a6a48adbe8a3a
Axelhri <axelhouairi@gmail.com>
2025-07-29 11:15:34 +0200

    feat(Publication): created publication entity and repository

---
+++ src/Entity/Publication.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PublicationRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: PublicationRepository::class)]
+class Publication
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id = null;
+
+    #[ORM\Column(type: 'string' ,length: 250)]
+    private ?string $text = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private ?bool $pin = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(?string $text): static
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function isPin(): ?bool
+    {
+        return $this->pin;
+    }
+
+    public function setPin(bool $pin): static
+    {
+        $this->pin = $pin;
+
+        return $this;
+    }
+}
