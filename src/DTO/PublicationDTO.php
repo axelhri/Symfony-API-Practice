e252201c9929cfc1073c31882d746c29de6ee902
Axelhri <axelhouairi@gmail.com>
2025-07-29 22:26:06 +0200

    feat(PublicationDTO): created dto

---
+++ src/DTO/PublicationDTO.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\DTO;
+
+use App\Entity\User;
+use Symfony\Component\Uid\Uuid;
+
+class PublicationDTO {
+    private ?Uuid $id;
+    private ?string $text;
+    private ?bool $pin;
+    private ?User $author;
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+
+    public function setId(?Uuid $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(?string $text): void
+    {
+        $this->text = $text;
+    }
+
+    public function getPin(): ?bool
+    {
+        return $this->pin;
+    }
+
+    public function setPin(?bool $pin): void
+    {
+        $this->pin = $pin;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): void
+    {
+        $this->author = $author;
+    }
+}
