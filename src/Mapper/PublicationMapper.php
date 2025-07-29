aa8d9bb43780397d40b7ff993eaeb5b54a5d5bd7
Axelhri <axelhouairi@gmail.com>
2025-07-29 22:39:50 +0200

    feat(PublicationMapper): to dto method

--- src/Mapper/PublicationMapper.php
+++ src/Mapper/PublicationMapper.php
@@ -7,13 +7,20 @@ use App\Entity\Publication;
 
 class PublicationMapper {
 
-    public function publicationToEntity(PublicationDTO $publicationDTO): Publication
-    {
+    public function publicationToEntity(PublicationDTO $publicationDTO): Publication {
         $publication = new Publication();
         $publication->setText($publicationDTO->getText());
         $publication->setPin($publicationDTO->getPin());
         $publication->setAuthor($publicationDTO->getAuthor());
         return $publication;
+    }
+
+    public function publicationToDTO(Publication $publication): PublicationDTO {
+        $publicationDTO = new PublicationDTO();
+        $publicationDTO->setText($publication->getText());
+        $publicationDTO->setPin($publication->isPin());
+        $publicationDTO->setAuthor($publication->getAuthor());
+        return $publicationDTO;
     }
 
 }
